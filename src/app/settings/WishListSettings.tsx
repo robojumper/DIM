26683995810a6a21cc07e379efedd2b56cc8444e
robojumper <robojumper@gmail.com>
2022-01-21 17:40:03 +0100

    WIP wish list validation

--- src/app/settings/WishListSettings.tsx
+++ src/app/settings/WishListSettings.tsx
@@ -1,9 +1,15 @@
 import { settingSelector } from 'app/dim-api/selectors';
 import { t } from 'app/i18next-t';
+import { DimItem } from 'app/inventory/item-types';
+import { bucketsSelector } from 'app/inventory/selectors';
+import { makeFakeItem } from 'app/inventory/store/d2-item-factory';
+import { useD2Definitions } from 'app/manifest/selectors';
 import { showNotification } from 'app/notifications/notifications';
 import { useThunkDispatch } from 'app/store/thunk-dispatch';
+import { WishListRoll } from 'app/wishlists/types';
 import { fetchWishList, transformAndStoreWishList } from 'app/wishlists/wishlist-fetch';
 import { toWishList } from 'app/wishlists/wishlist-file';
+import _ from 'lodash';
 import React, { useEffect, useState } from 'react';
 import { DropzoneOptions } from 'react-dropzone';
 import { useSelector } from 'react-redux';
@@ -43,6 +49,8 @@ export default function WishListSettings() {
   useEffect(() => {
     setLiveWishListSource(wishListSource);
   }, [wishListSource]);
+
+  const [validate, setValidate] = useState<boolean>(false);
 
   const reloadWishList = async (reloadWishListSource: string | undefined) => {
     try {
@@ -87,6 +95,10 @@ export default function WishListSettings() {
   const clearWishListEvent = () => {
     ga('send', 'event', 'WishList', 'Clear');
     dispatch(clearWishLists());
+  };
+
+  const validateWishListEvent = () => {
+    setValidate(true);
   };
 
   const resetToChoosyVoltron = () => {
@@ -182,6 +194,11 @@ export default function WishListSettings() {
             <button type="button" className="dim-button" onClick={clearWishListEvent}>
               {t('WishListRoll.Clear')}
             </button>
+            {$DIM_FLAVOR === 'dev' && (
+              <button type="button" className="dim-button" onClick={validateWishListEvent}>
+                Validate
+              </button>
+            )}
           </div>
           {wishList.infos.map(({ title, description, numRolls }, idx) => (
             <div className="fineprint" key={idx}>
@@ -194,6 +211,151 @@ export default function WishListSettings() {
           ))}
         </div>
       )}
+
+      {wishListSource && validate && (
+        <div className="setting">
+          <WishlistValidation rolls={wishList.wishListRolls} />
+        </div>
+      )}
     </section>
+  );
+}
+
+type ErrorCode =
+  | { code: 'missing_hash' }
+  | { code: 'missing_perk'; perkHash: number }
+  | { code: 'perk_columns'; perksHashes: number[]; numCols: number };
+
+type Error = { itemHash: number; code: ErrorCode };
+
+function WishlistValidation({ rolls }: { rolls: WishListRoll[] }) {
+  const defs = useD2Definitions()!;
+  const buckets = useSelector(bucketsSelector)!;
+
+  const fakeItems: { [key: number]: DimItem } = {};
+  const errors: Error[] = [];
+
+  for (const roll of rolls) {
+    if (!fakeItems[roll.itemHash]) {
+      const fakeItem = makeFakeItem(defs, buckets, undefined, roll.itemHash);
+      if (!fakeItem) {
+        errors.push({ itemHash: roll.itemHash, code: { code: 'missing_hash' } });
+        continue;
+      }
+      fakeItems[roll.itemHash] = fakeItem;
+    }
+
+    const fakeItem = fakeItems[roll.itemHash]!;
+    const findSocketIndices = (perkHash: number) =>
+      fakeItem.sockets?.allSockets
+        .filter((s) => s.plugOptions.some((p) => perkHash === p.plugDef.hash))
+        .map((s) => s.socketIndex);
+
+    const perksPerSocket: Map<number, number[]> = new Map();
+
+    for (const perk of roll.recommendedPerks) {
+      const indices = findSocketIndices(perk);
+      if (!indices || !indices.length) {
+        errors.push({ itemHash: roll.itemHash, code: { code: 'missing_perk', perkHash: perk } });
+        continue;
+      }
+
+      indices.forEach((i) => {
+        if (!perksPerSocket.get(i)) {
+          perksPerSocket.set(i, []);
+        }
+        perksPerSocket.get(i)!.push(perk);
+      });
+    }
+
+    // Remove columns with only a single valid wishlisted perk
+    const socketPerks = Array.from(perksPerSocket.entries());
+    for (const [idx, perks] of perksPerSocket.entries()) {
+      if (
+        perks.length === 1 &&
+        socketPerks.every(
+          ([otherIdx, otherPerks]) => idx === otherIdx || !otherPerks.includes(perks[0])
+        )
+      ) {
+        perksPerSocket.delete(idx);
+      }
+    }
+
+    const numSockets = perksPerSocket.size;
+    const perks = _.uniq(Array.from(perksPerSocket.values()).flatMap((x) => x));
+
+    if (numSockets < perks.length) {
+      errors.push({
+        itemHash: roll.itemHash,
+        code: {
+          code: 'perk_columns',
+          perksHashes: [...perks].sort(),
+          numCols: numSockets,
+        },
+      });
+    }
+  }
+
+  const unique = _.uniqWith(errors, (l: Error, r: Error) => _.isEqual(l, r));
+
+  const formatItem = (itemHash: number) => {
+    const item = defs.InventoryItem.get(itemHash);
+    return (
+      <a href={`https://data.destinysets.com/i/InventoryItem:${itemHash}`}>
+        {item?.displayProperties.name || itemHash}
+      </a>
+    );
+  };
+
+  const formatError = (e: Error) => {
+    switch (e.code.code) {
+      case 'missing_hash': {
+        return <span>Item not found</span>;
+      }
+      case 'missing_perk': {
+        return (
+          <>
+            <span>Perk </span>
+            {formatItem(e.code.perkHash)}
+            <span> not found</span>
+          </>
+        );
+      }
+      case 'perk_columns': {
+        return (
+          <>
+            <span>{`${e.code.perksHashes.length} perks `}</span>
+            {e.code.perksHashes.map((perk, idx) => (
+              <span key={idx}>
+                {idx > 0 && ', '}
+                {formatItem(perk)}
+              </span>
+            ))}
+            <span>{` only roll in ${e.code.numCols} different columns`}</span>
+          </>
+        );
+      }
+    }
+  };
+
+  const borders = { border: '1px solid grey', borderCollapse: 'collapse' } as const;
+
+  return (
+    <table style={borders}>
+      <thead>
+        <tr>
+          <th style={borders}>Item</th>
+          <th style={borders}>Message</th>
+        </tr>
+      </thead>
+      <tbody>
+        {unique.map((e, idx) => (
+          <tr key={idx}>
+            <td style={borders}>{formatItem(e.itemHash)}</td>
+            <td style={{ whiteSpace: 'normal', ...borders }}>{formatError(e)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
